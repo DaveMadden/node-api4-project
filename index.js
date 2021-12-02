11958bb91df2f21d98afe4ed06a53a66a3af298e
Dave Madden <davelearnsstuff@gmail.com>
2021-12-02 14:35:01 -0800

    wip

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ server.get('/api/users', (req, res)=>{
     ])
 })
 
+server.post('/api/register', (req, res)=>{
+    res.json({message: `registered`})
+})
+
+server.post('/api/login', (req, res)=>{
+    res.json({message: `welcome!`})
+})
+
+// server.get('/', (req,res)=>{
+//     res.send("try adding '/api/users' to the url!")
+// })
+// server.get('*', (req,res)=>{
+//     res.send("that's not a valid path but you're still a worthwhile person and i appreciate you")
+// })
+
+const PORT = process.env.PORT || 8000
+
+server.listen(PORT, ()=>{console.log("listening on port ", PORT)})
